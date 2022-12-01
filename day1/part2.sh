284585fb20612d00705bd150f6c721480534f237
Mrat <78581772+mr4tt@users.noreply.github.com>
2022-12-01 11:54:29 -0800

    finished pt2

--- day1/part2.sh
+++ day1/part2.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-# input=input.txt
-input=test.txt
+input=input.txt
+# input=test.txt
 
-declare {elf,firstElf,secondElf,thirdElf}=1
 declare {sum,firstSum,secondSum,thirdSum}=0
-
 
 while IFS= read -r line; do	
 # 	echo $line
 	if [ -n "$line" ]; then
 		sum=$((sum+line))
 	else
-		if [ $sum -gt $firstSum ]; then
+		if [ "$sum" -gt "$firstSum" ]; then
+			thirdSum=$secondSum
+			secondSum=$firstSum
+
 			firstSum=$sum
-			firstElf=$elf
-		elif [ $sum -gt $secondSum ]; then
+		elif [ "$sum" -gt "$secondSum" ]; then
+			thirdSum=$secondSum
+
 			secondSum=$sum
-			secondElf=$elf
-		elif [ $sum -gt $thirdSum ]; then
+		elif [ "$sum" -gt "$thirdSum" ]; then
 			thirdSum=$sum
-			thirdElf=$elf
 		fi
-		((elf++))
-# 		echo $elf
 		sum=0
 	fi
  
-done < "$input" && echo $firstSum + $secondSum + $thirdSum, $firstElf, $secondElf, $thirdElf
+done < "$input" && echo "$firstSum" + "$secondSum" + "$thirdSum"
 
+#done < "$input" && echo $((firstSum+secondSum+thirdSum))
+
+echo $((firstSum+secondSum+thirdSum))
 
 
